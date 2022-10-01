42fa0248dfb603604b4311101011db369ed567fd
Carlos Vinicius Rocha Guedes <c.vinicius.gr@gmail.com>
2022-10-01 12:59:10 -0300

    Add project files.

---
+++ SistemaWebCooperativa/Models/Cooperado.cs
@@ -0,0 +1,12 @@
+﻿namespace SistemaWebCooperativa.Models
+{
+    public class Cooperado
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string telefone { get; set; }
+        public string email { get; set; }
+        public int cpf { get; set; }
+        public Propriedade propriedade { get; set; }
+    }
+}
---
+++ SistemaWebCooperativa/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemaWebCooperativa.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
